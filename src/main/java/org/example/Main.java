04814ffbe0caf318774ebd74f97dfa8486eb214d
Beopsik <mbsik6082@naver.com>
2023-02-07 14:13:31 +0900

    Add kStreamJoinTable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 //        SimpleStreamsApplication.run();
-        StreamsFilter.run();
+//        StreamsFilter.run();
+        kStreamJoinTable.run();
     }
 }
