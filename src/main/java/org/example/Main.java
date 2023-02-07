47efdd6fcf01d9928360d24eff710d7200bad11c
Beopsik <mbsik6082@naver.com>
2023-02-07 13:55:04 +0900

    Add StreamsFilter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleStreamsApplication.run();
+//        SimpleStreamsApplication.run();
+        StreamsFilter.run();
     }
 }
