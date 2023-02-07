46d7e336d1898cd380615981bf2369477bac2875
Beopsik <mbsik6082@naver.com>
2023-02-07 15:48:56 +0900

    processor api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.processor.SimpleKafkaProcessor;
+
 public class Main {
     public static void main(String[] args) {
 //        SimpleStreamsApplication.run();
 //        StreamsFilter.run();
 //        kStreamJoinTable.run();
-        KStreamJoinGlobalKTable.run();
+//        KStreamJoinGlobalKTable.run();
+        SimpleKafkaProcessor.run();
     }
 }
